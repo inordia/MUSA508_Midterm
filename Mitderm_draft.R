ea4d5e9bc2e323abfd10dcf728da6d209b2b3ede
inordia <rw1932@nyu.edu>
2021-10-03 17:26:39 -0400

    crime and park added

--- Mitderm_draft.R
+++ Mitderm_draft.R
@@ -12,6 +12,7 @@ library(ggcorrplot)
 library(kableExtra)
 library(jtools)     # for regression model plots
 library(ggstance) # to support jtools plots
+library(mapview)
 
 root.dir = "https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-Landing/master/DATA/"
 
@@ -20,6 +21,11 @@ source("https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-L
 palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
 
 ##Housing Data
+
+Boulder_boundary <- st_read("/Users/inordia/Desktop/UPenn搞起来/592/MUSA508_Midterm/City_of_Boulder_City_Limits.kml") %>%
+  st_transform('EPSG:26916')%>%
+  select(-Name, -Description)
+Boulder_boundary <- st_union(Boulder_boundary)
 
 parcel_account <- read.csv("/Users/inordia/Desktop/UPenn搞起来/592/MUSA508_Midterm/Account_Parcels.csv")
 building <- read.csv("/Users/inordia/Desktop/UPenn搞起来/592/MUSA508_Midterm/Buildings.csv")
@@ -34,5 +40,20 @@ house <- building %>%
   left_join(.,parcel_account, by=c("strap"="strap"))%>%
   left_join(.,parcel_gis, by=c("Parcelno"="PARCEL_NO"))%>%
   st_sf()%>%
-  select(-deedNum, -status_cd.y)
+  select(-deedNum, -status_cd.y)%>%
+  st_intersection(house,Boulder_boundary)
+
+##Crime Data
+  
+crime <- st_read("/Users/inordia/Desktop/UPenn搞起来/592/MUSA508_Midterm/Boulder_Police_Department_(BPD)_Offenses.geojson") %>%
+  st_transform('EPSG:26916')%>%
+  filter(crime, Report_Year=="2019")
+
+##Public Facilities
+
+park <- st_read("/Users/inordia/Desktop/UPenn搞起来/592/MUSA508_Midterm/Properties_Managed_by_Parks_and_Recreation_Polygons.geojson")%>%
+  st_transform('EPSG:26916')%>%
+  select(NAME, PARKTYPE, SUBCOMMUNITY, geometry)
+
+
 
